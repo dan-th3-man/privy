a8b9ad63a8a97c974efdb7ce6d57e812d012e3c7
dansmith4400 <dan.smith2000@hotmail.co.uk>
2024-08-01 01:34:49 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
 import { usePrivy } from "@privy-io/react-auth"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const { login, user } = usePrivy();
+  const { login } = usePrivy();
 
   return (
     <>
